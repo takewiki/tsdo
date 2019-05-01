92e24fe3a2b24726b7186ffbe60aa8408f8beca9
takewiki <hulilei@takewiki.com.cn>
2019-05-01 18:10:57 +0800

    delete the tsdo.R file

--- R/01-data_frame.R
+++ R/01-data_frame.R
@@ -1,4 +1,5 @@
-#' 返回数据值的每列的 
+#  返回数据值的每列的数据类型 ----
+#' 返回数据值的每列的数据类型 
 #'
 #' @param data_frame 数据框
 #'
@@ -7,7 +8,26 @@
 #'
 #' @examples 
 df_columnType <- function(data_frame) {
-  res <- unlist(lapply(tensorFrameExample, class));
+  if (class(data_frame) != 'data.frame'){
+    stop('数据类型必须为data.frame！',call. = FALSE)
+  }
+  res <- unlist(lapply(data_frame, class));
   return(res);
-  
 }
+
+#' 获取数据框的列名
+#'
+#' @param data_frame 数据框
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples df_columnName()
+df_columnName <- function(data_frame) {
+  if (class(data_frame) != 'data.frame'){
+    stop('数据类型必须为data.frame！',call. = FALSE)
+  }
+  res <- names(data_frame)
+  return(res);
+}
+
--- R/tsdo.R
+++
@@ -1,3 +0,0 @@
-#' add the function 
-# functions for the data.frame----
-NULL
