14a12523532e5d83566343d656c2068dd4fa8544
takewiki <hulilei@takewiki.com.cn>
2020-11-01 23:43:28 +0800

    v3.06A

--- R/00-R_utils.R
+++ R/00-R_utils.R
@@ -14,13 +14,14 @@ R_expr <- function(string){
 #' 执行R代表
 #'
 #' @param expr 表达式
+#' @param envir 环境
 #'
 #' @return 执行结果，不需要返回
 #' @export
 #'
 #' @examples R_exec();
-R_exec <- function(expr){
-  eval(expr = expr,envir = parent.frame())
+R_exec <- function(expr,envir = parent.frame()){
+  eval(expr = expr,envir = envir)
 }
 # 引入相应的管道符号
 #' @importFrom magrittr %>%
@@ -40,7 +41,10 @@ magrittr::`%>%`
 callR <- function(txt) {
   
   expr_1 <- R_expr(txt)
-  R_exec(expr = expr_1)
+  envir = parent.env(parent.frame())
+  print(envir)
+  R_exec(expr = expr_1,envir = envir)
+
   return(res)
   
 }
